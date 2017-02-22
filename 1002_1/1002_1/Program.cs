94502b379685dbeab22ac3e3cf8b0a89505a542b
hanimanaa <hanimanaa@gmail.com>
2017-02-22 11:02:36 +0200

    תרגיל 1

---
+++ 1002_1/1002_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double age;
+
+            Console.WriteLine(" ad5el name : ");
+            name = Console.ReadLine();
+            Console.WriteLine(" ad5el age : ");
+            age = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " 3omroho " + age + " snen :P");
+
+            Console.ReadKey();
+        }
+    }
+}
