93e36299b81cfb3af26f65731475d3d919164c52
hanimanaa <hanimanaa@gmail.com>
2017-02-22 11:16:21 +0200

    switch

---
+++ 1002_5/1002_5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, t;
+            char ch;
+
+            Console.WriteLine(" ad5el al3dad 1 : ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ad5el al3mlyah : ");
+            ch = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ad5el al3dad 2 : ");
+            y = double.Parse(Console.ReadLine());
+
+            switch (ch)
+            {
+                case '+':
+                    t = x + y;
+                    Console.WriteLine(" " + x + " + " + y + " = " + t);
+                    break;
+                case '-':
+                    t = x - y;
+                    Console.WriteLine(" " + x + " - " + y + " = " + t);
+                    break;
+                case '*':
+                    t = x * y;
+                    Console.WriteLine(" " + x + " * " + y + " = " + t);
+                    break;
+                case '/':
+                    t = x / y;
+                    Console.WriteLine(" " + x + " / " + y + " = " + t);
+                    break;
+                default:
+                    Console.WriteLine("Error !!!");
+                    break;
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
