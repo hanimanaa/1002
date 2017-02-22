c7774b22e0d3f45ef951081e3e2818fe28d65cd5
hanimanaa <hanimanaa@gmail.com>
2017-02-22 11:06:49 +0200

    if

--- 1002_2/1002_2/Program.cs
+++ 1002_2/1002_2/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1002_2
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name;
+            double age;
+
+            Console.WriteLine(" ad5el name : ");
+            name = Console.ReadLine();
+            Console.WriteLine(" ad5el age : ");
+            age = double.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine(name + " 7maaaar :p");
+            }
+            else
+            {
+                Console.WriteLine(name + " msh 7maaaar :D");
+            }
+
+
+            Console.ReadKey(); // ---
+
         }
     }
 }
